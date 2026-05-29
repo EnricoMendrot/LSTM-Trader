4e1f8c65b24497618196fb959f80106d4d854613
Enrico Mendrot <enricomendrot23@gmail.com>
2026-05-29 20:54:30 -0300

    feat: adicionado o método de login com JWT básico

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -1,14 +1,30 @@
 from schemas.user import UserSchema
+from schemas.login import LoginSchema
 from fastapi import APIRouter, Depends, HTTPException
 from models.models import User
 from sqlalchemy.orm import Session
-from dependencies import get_session, get_password_hash
+from dependencies import get_session, get_password_hash, verify_password
 
 
-auth = APIRouter(prefix="/login", tags=["login"])
+auth = APIRouter(prefix="/auth", tags=["auth"])
 
+# ======== Funções auxiliares para autenticação ======== #
+def gerenate_token(user: User.id_user) -> int:
+    token = f"tgverfvirwjovo{user}"
+    return token
+
+def verify_user(email, password, session):
+    user = session.query(User).filter(User.email == email).first()
+    if not user:
+        return False
+    elif not verify_password(password, user.password_hash):
+        return False
+    return user
+
+
+# ========= Endpoints de autenticação ========= #
 @auth.post("/create")
-def create_user(usuario_schema: UserSchema, session: Session = Depends(get_session)):
+async def create_user(usuario_schema: UserSchema, session: Session = Depends(get_session)):
     """
     Endpoint para criar/cadastrar um novo usuário.
     """
@@ -22,3 +38,16 @@ def create_user(usuario_schema: UserSchema, session: Session = Depends(get_sessi
         session.add(new_user)
         session.commit()
         return {"message": "User created successfully"}
+
+
+@auth.post("/login")
+async def login_user(login_schema: LoginSchema, session: Session = Depends(get_session)):
+    """
+    
+    """
+    user = verify_user(login_schema.email, login_schema.password, session)
+    if not user:
+        raise HTTPException(status_code=400, detail="Invalid email or password")
+    else:
+        access_token = gerenate_token(user)
+        return {"acess_token": access_token, "token_type": "Bearer"}
